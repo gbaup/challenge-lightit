b0337b15939724cafccc8d70f52b6ebbe1002863
gastonbauer <gastonbauerp@gmail.com>
2025-01-14 22:10:11 -0300

    feat: missing files in github

---
+++ src/patient/dto/create-patient.dto.ts
@@ -0,0 +1,21 @@
+import { IsRequiredString } from '../../common/decorators/is-required-string.decorator';
+import { IsEmail, IsNotEmpty, IsPhoneNumber } from 'class-validator';
+
+export class CreatePatientDto {
+  @IsRequiredString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsRequiredString()
+  address: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsRequiredString()
+  photo: string;
+}
